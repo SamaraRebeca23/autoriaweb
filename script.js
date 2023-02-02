dba8e77b79c168de4539a14370cd7543e8d910fa
SammaraRebeca23 <samara.rebeca@escolar.ifrn.edu.br>
2023-02-02 11:52:25 -0300

    modo escuro

---
+++ script.js
@@ -0,0 +1,10 @@
+let section = document.querySelector("section") 
+
+function toDarkSide(){
+    section.classList.add("dark")
+    section.classList.remove("light")
+}
+function toDarkSide(){
+    section.classList.add("light")
+    section.classList.toggle("dark")
+}   
